e5e66e549484fd4ca5f511daa56ced3cbfc73903
Alexina Chong <alexina.chong@gmail.com>
2018-03-24 00:10:37 -0700

    Implement Judge Route Circle

--- leetcode/practice_set.rb
+++ leetcode/practice_set.rb
@@ -21,3 +21,27 @@ def num_jewels_in_stones(j, s)
     end
     count
 end
+
+# Q595: Big Countries
+# SELECT name, population, area
+# FROM World
+# WHERE area > 3000000 OR population > 25000000
+
+# Q657: Judge Route Circle
+def judge_circle(moves)
+    vert_offset = 0
+    horiz_offset = 0
+    moves.each_char do |move|
+        if move == "U"
+            vert_offset += 1
+        elsif move == "D"
+            vert_offset -= 1
+        elsif move == "L"
+            horiz_offset -= 1
+        elsif move == "R"
+            horiz_offset += 1
+        end
+    end
+    return true if vert_offset == 0 && horiz_offset == 0
+    false
+end
