91838bdab3f4b4616a97f8b8748fba35b3715ffa
Alexina Chong <alexina.chong@gmail.com>
2018-06-28 23:29:39 -0700

    Implement SQL ProjectList problem

---
+++ codefights/practice_set.rb
@@ -0,0 +1,10 @@
+# Project List
+# CREATE PROCEDURE projectList()
+# BEGIN
+# 	SELECT
+#         project_name, team_lead, income
+#     FROM
+#         Projects
+#     ORDER BY
+#         internal_id ASC;
+# END
