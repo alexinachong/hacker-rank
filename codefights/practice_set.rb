016b21f3a994fe96a3390885fa17c2206516a42e
Alexina Chong <alexina.chong@gmail.com>
2018-06-28 23:36:51 -0700

    Implement SQL Monthly Scholarships problem

--- codefights/practice_set.rb
+++ codefights/practice_set.rb
@@ -21,3 +21,12 @@
 #     ORDER BY
 #         name ASC;
 # END
+
+# Monthly Scholarships
+# CREATE PROCEDURE monthlyScholarships()
+# BEGIN
+# 	SELECT
+#         id, scholarship / 12 AS scholarship
+#     FROM
+#         scholarships;
+# END
