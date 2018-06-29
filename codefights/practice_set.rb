f45e51bedc73f666c5598356c1e8dcf8b3fbd450
Alexina Chong <alexina.chong@gmail.com>
2018-06-28 23:32:44 -0700

    Implement SQL CountriesSelection problem

--- codefights/practice_set.rb
+++ codefights/practice_set.rb
@@ -8,3 +8,16 @@
 #     ORDER BY
 #         internal_id ASC;
 # END
+
+# Countries Selection
+# CREATE PROCEDURE countriesSelection()
+# BEGIN
+# 	SELECT
+#         *
+#     FROM
+#         countries
+#     WHERE
+#         continent = 'Africa'
+#     ORDER BY
+#         name ASC;
+# END
