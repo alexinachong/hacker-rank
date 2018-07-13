766523f282e5d6ecaa70351d071b7a2bfee1407a
Alexina Chong <alexina.chong@gmail.com>
2018-07-12 21:38:49 -0700

    Implement centuryFromYear

--- codefights/practice_set.rb
+++ codefights/practice_set.rb
@@ -75,3 +75,13 @@ def amendTheSentence(s)
 
     result.strip
 end
+
+def centuryFromYear(year)
+    century = year / 100
+
+    if year % 100 == 0
+        return century
+    else
+        return century + 1
+    end
+end
